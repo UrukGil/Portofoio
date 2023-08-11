2802dd89e011fc632de97e577e32ac3e2c9fd625
UrukGil <135685808+UrukGil@users.noreply.github.com>
2023-08-10 23:39:38 -0700

    new index

---
+++ js/style.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const carousel = document.querySelector('.carousel');
+//   const icon = document.querySelector('.carousel_icon');
+
+//   // 切换旋转木马状态
+//   icon.addEventListener('click', function() {
+//       carousel.classList.toggle('show-second-carousel');
+//   });
+// });
