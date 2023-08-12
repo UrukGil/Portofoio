f1c1b0b0b07b45a17952117f92102393f5099a30
UrukGil <135685808+UrukGil@users.noreply.github.com>
2023-08-11 23:32:29 -0700

    SIAT Responsive

--- js/style.js
+++ js/style.js
@@ -9,3 +9,14 @@
 //       carousel.classList.toggle('show-second-carousel');
 //   });
 // });
+
+const filmStrip = document.querySelector('.film-strip');
+
+let images = filmStrip.querySelectorAll('img');
+let totalWidth = 0;
+
+images.forEach(img => {
+    totalWidth += img.clientWidth + 20; // 20px 是图片之间的间距
+});
+
+filmStrip.style.width = totalWidth + 'px';
